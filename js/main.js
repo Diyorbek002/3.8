043184b54c4e19d87e7dcc82d6dfd1e124e2e035
Diyorbek002 <zamonbekovdiyorbek98@gamil.com>
2022-02-26 16:16:36 +0500

    Initial commit

--- js/main.js
+++ js/main.js
@@ -10,19 +10,43 @@ var elSpanCar = document.querySelector(".box__car")
 
 var elSpanAiroplan = document.querySelector(".box__airoplan")
 
+
+
+
+
 elForm.addEventListener("submit" , function (event){
     event.preventDefault()
-    
+
     var elInputVal = elInput.value;
+
     var SpanMan = 3.6;
+
     var SpanVelo = 20;
+
     var SpanCar = 70;
+
     var SpanAiroplan = 800;
+
+    function man () {
+            return elInputVal / SpanMan
+    }
+
+    function velo () {
+            return elInputVal / SpanVelo
+    }
     
-    elSpanMan.textContent = Math.round(elInputVal / SpanMan);
-    elSpanVelo.textContent = Math.round(elInputVal / SpanVelo);
-    elSpanCar.textContent = Math.round(elInputVal / SpanCar);
-    elSpanAiroplan.textContent = Math.round(elInputVal / SpanAiroplan);
+    function car () {
+            return elInputVal / SpanCar
+    }
+    
+    function airoplan () {
+            return elInputVal / SpanAiroplan
+    }
+    
+    elSpanMan.textContent = man().toFixed(2)
+    elSpanVelo.textContent = velo().toFixed(2)
+    elSpanCar.textContent = car().toFixed(2)
+    elSpanAiroplan.textContent =airoplan().toFixed(2)
     
 })
 
