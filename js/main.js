29e458d679dc75d2bd0c1e906bc4cc583c536b8a
Diyorbek002 <zamonbekovdiyorbek98@gamil.com>
2022-03-02 21:12:15 +0500

    Initial commit

--- js/main.js
+++ js/main.js
@@ -1,3 +1,11 @@
+var SpanMan = 3.6;
+
+var SpanVelo = 20;
+
+var SpanCar = 70;
+
+var SpanAiroplan = 800;
+
 var elForm = document.querySelector(".header__form")
 
 var elInput = document.querySelector(".header__input")
@@ -10,43 +18,37 @@ var elSpanCar = document.querySelector(".box__car")
 
 var elSpanAiroplan = document.querySelector(".box__airoplan")
 
+var elSpan = document.querySelector(".header__span")
 
+elForm.addEventListener("submit" , function (evt){
+        evt.preventDefault()
 
+var elInputVal = Number(elInput.value.trim());
+        function man () {
+        var hour = Math.floor (elInputVal / SpanMan);
+        var minut = Math.floor( (elInputVal / SpanMan - hour )*60);
+        return hour + " soat" + minut + "minut"}
 
+        function velo () {
+        var hour = Math.floor (elInputVal / SpanVelo);
+        var minut = Math.floor( (elInputVal / SpanVelo - hour )*60);
+        return hour + " soat" + minut + "minut"}
 
-elForm.addEventListener("submit" , function (event){
-    event.preventDefault()
+        function car () {
+        var hour = Math.floor (elInputVal / SpanCar);
+        var minut = Math.floor( (elInputVal / SpanCar - hour )*60);
+        return hour + " soat" + minut + "minut"}
 
-    var elInputVal = elInput.value;
+        function airoplan () {
+        var hour = Math.floor (elInputVal / SpanAiroplan);
+        var minut = Math.floor( (elInputVal / SpanAiroplan - hour )*60);
+        return hour + " soat" + minut + "minut"
 
-    var SpanMan = 3.6;
-
-    var SpanVelo = 20;
-
-    var SpanCar = 70;
-
-    var SpanAiroplan = 800;
-
-    function man () {
-            return elInputVal / SpanMan
-    }
-
-    function velo () {
-            return elInputVal / SpanVelo
-    }
-    
-    function car () {
-            return elInputVal / SpanCar
-    }
-    
-    function airoplan () {
-            return elInputVal / SpanAiroplan
-    }
-    
-    elSpanMan.textContent = man().toFixed(2)
-    elSpanVelo.textContent = velo().toFixed(2)
-    elSpanCar.textContent = car().toFixed(2)
-    elSpanAiroplan.textContent =airoplan().toFixed(2)
-    
-})
+}
+                
+    elSpanMan.textContent = man()
+    elSpanVelo.textContent = velo()
+    elSpanCar.textContent = car()
+    elSpanAiroplan.textContent = airoplan()
+ })
 
